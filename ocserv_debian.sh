0d3fc76fa7e26b84fb17e0c271401925fafa3176
githik999 <kakaxi77749@gmail.com>
2022-05-19 06:42:24 +0800

    Update ocserv_debian.sh

--- ocserv_debian.sh
+++ ocserv_debian.sh
@@ -1,3 +1,11 @@
+create_cert(){
+	curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+	chmod +x mkcert-v*-linux-amd64
+	sudo cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+
+	mkcert -key-file /etc/ocserv/private.key -cert-file /etc/ocserv/public.cert $1
+}
+
 tmp=`cat /etc/debian_version`
 ver=${tmp: 0: 1}
 if [ "$ver" -gt "1" ];then
@@ -18,11 +26,12 @@ echo ":3389(Port MUST Not Blocked)" >> /etc/ocserv/server.address
 curl -o /etc/ocserv/ocserv.conf https://gitee.com/chrisrock/ocserv_one_key/raw/main/ocserv.conf
 echo "no-route=$public_ip/24" >> /etc/ocserv/ocserv.conf
 
-curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
-chmod +x mkcert-v*-linux-amd64
-sudo cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+if [ ! -f "private.key" ];then
+  echo "already have private.key mkcert will skip"
+else
+  create_cert $public_ip
+fi
 
-mkcert -key-file /etc/ocserv/key.pem -cert-file /etc/ocserv/cert.pem $public_ip
 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
